f8e9400f748b6594e2e5c564ef2b3f08fb86bec9
Kevin Funke <kev.funke@gmail.com>
2021-01-23 13:04:18 +0100

    added attr reader

--- lib/gosu_tiled/map.rb
+++ lib/gosu_tiled/map.rb
@@ -1,21 +1,27 @@
 module Gosu
   module Tiled
     class Map
-      attr_reader :tilesets, :layers, :width, :height
+      attr_reader :tilesets, :layers, :width, :height, :tilewidth, :tileheight, :pixelwidth, :pixelheight
 
       def initialize(window, data, data_dir, options = {})
+        zoom = options[:zoom] || 1
+        grid = options[:grid] || false
         @window = window
         @data = data
         @data_dir = data_dir
-        @width = data['width'] * data['tilewidth']
-        @height = data['height'] * data['tileheight']
+        @tilewidth = data['tilewidth'] * zoom
+        @tileheight = data['tileheight'] * zoom
+        @width = data['width']
+        @height = data['height']
+        @pixelwidth = data['width'] * @tilewidth
+        @pixelheight = data['height'] * @tileheight
+
         @tilesets = Tilesets.new(data['tilesets'], data_dir)
-        zoom = options[:zoom] || 1
-        grid = options[:grid] || false
+
         @layers = Layers.new(window,
                              data['layers'],
-                             width: @width,
-                             height: @height,
+                             width: @pixelwidth,
+                             height: @pixelheight,
                              tile_width: data['tilewidth'],
                              tile_height: data['tileheight'], zoom: zoom, grid: grid)
       end
